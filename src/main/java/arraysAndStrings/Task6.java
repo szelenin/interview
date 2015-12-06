cbc0f617c594c1958ed64d45b83800eb98261d5c
szelenin <szelenin@playtika.com>
2015-12-06 16:29:31 +0200

    Arrays and Strings Task 6 - Fails again

--- src/main/java/arraysAndStrings/Task6.java
+++ src/main/java/arraysAndStrings/Task6.java
@@ -4,19 +4,26 @@ package arraysAndStrings;
  * Created by szelenin on 12/5/2015.
  */
 public class Task6 {
-    public static int[][] rotate(int[][] image){
-        int n = image.length;
-        for (int i = 0;i<n/2;i++){
-            int upperLeftPixel = image[i][i];
-            int upperRightPixel = image[i][n-i-1];
-            int lowerRightPixel = image[n-i-1][n-i-1];
-            int lowerLeftPixel = image[n-i-1][i];
-            image[i][i] =  lowerLeftPixel;
-            image[i][n-i-1] = upperLeftPixel;
-            image[n-i-1][n-i-1] = upperRightPixel;
-            image[n-i-1][i] = lowerRightPixel;
+    public static int[][] rotate(int[][] image) {
+        for (int j = 0; j < image.length / 2; j++) {
+            rotateLine(j, image);
         }
         return image;
+    }
+
+    private static void rotateLine(int j, int[][] image) {
+        int n = image.length;
+        for (int i = j; i < n - 1 - j; i++) {
+            int ul = image[j][i];
+            int ur = image[i][n-1-j];
+            int lr = image[n-1-j][n-1-j-i];
+            int ll = image[n-1-j-i][j];
+            image[j][i]=ll;
+            image[i][n-1-j]=ul;
+            image[n-1-j][n-1-j-i]=ur;
+            image[n-1-j-i][j]=lr;
+        }
+
     }
 }
 
