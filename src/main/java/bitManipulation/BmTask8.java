e93f9df3a4b0960ec8b396bcb5f3437abe137707
szelenin <szelenin@playtika.com>
2016-02-07 16:06:39 +0200

    Bit manipulation task 8 : errors

---
+++ src/main/java/bitManipulation/BmTask8.java
@@ -0,0 +1,38 @@
+package bitManipulation;
+
+/**
+ * Created by szelenin on 2/7/2016.
+ */
+public class BmTask8 {
+    public static byte[] drawLine(int x1, int x2, int y, int w, byte[] a) {
+        int h = a.length / w;
+        if (y > h - 1) {
+            y = h - 1;
+        }
+        if (x1 > x2) {
+            int tmp = x1;
+            x1=x2;
+            x2 = tmp;
+        }
+        if (x1 > w * 8 - 1) {
+            x1 = w * 8 - 1;
+        }
+        if (x2 > w * 8 - 1) {
+            x2 = w * 8 - 1;
+        }
+        int lineLength = x2 - x1 + 1;
+        int startBit = x1 % 8;
+        int currentIdx = w * y + x1 / 8;
+        while (lineLength > 0) {
+            int bitLen = startBit + lineLength > 8 ? 8 - startBit : lineLength;
+            a[currentIdx] = drawPx(a[currentIdx], startBit, bitLen);
+            currentIdx++;
+            lineLength-=bitLen;
+        }
+        return a;
+    }
+
+    private static byte drawPx(byte a, int startBit, int len) {
+        return (byte) (a | 0xff >> startBit & 0xff << (8 - len));
+    }
+}
---
+++ src/test/java/bitManipulation/BmTask8Test.java
@@ -0,0 +1,29 @@
+package bitManipulation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by szelenin on 2/7/2016.
+ */
+public class BmTask8Test {
+    @Test
+    public void should(){
+        assertEquals((byte)0b11110000, draw(0, 3, 0, 2)[0]);
+        byte[] bytes = draw(8, 10, 0, 2);
+        assertEquals(0, bytes[0]);
+        assertEquals((byte)0b11100000, bytes[1]);
+    }
+    @Test
+    public void should2(){
+        byte[] bytes = draw(5, 10, 0, 2);
+        assertEquals((byte)0b00001111, bytes[0]);
+        assertEquals((byte)0b11100000, bytes[1]);
+    }
+
+    private byte[] draw(int x1, int x2, int y, int w) {
+        return BmTask8.drawLine(x1, x2, y, w, new byte[]{0, 0, 0, 0});
+    }
+
+}
