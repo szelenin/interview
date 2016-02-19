a65de3a5c19aef2fb562573884c5a8df8e53f9fa
szelenin <szelenin@playtika.com>
2016-02-19 12:55:03 +0200

    Iterative inorder (uber interview)

---
+++ src/main/java/treesAndGraphs/Main.java
@@ -0,0 +1,54 @@
+package treesAndGraphs;
+
+import java.util.*;
+
+// This is the text editor interface.
+// Anything you type or change here will be seen by the other person in real time.
+public class Main {
+    public static void main(String[] args) {
+//        TreeNode node = new TreeNode(1,  null, null);
+//        inOrder(node);
+        TreeNode node = new TreeNode(1, new TreeNode(2, null, null), new TreeNode(3, null, null));
+        inOrder(node);
+    }
+
+    public static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int data, TreeNode left, TreeNode right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void inOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        TreeNode current = node;
+        Stack<TreeNode> stack = new Stack<>();
+        do {
+            if (current != null) {
+                stack.push(current);
+                current = current.left;
+                continue;
+            }
+            current = stack.pop();
+            System.out.println(current.data);
+            current = current.right;
+            if (current != null) {
+                stack.push(current);
+            }
+        } while (!stack.isEmpty());
+    }
+
+//    inOrder(TreeNode node){
+//        check for null
+//        inOrder(node.left)
+//        visit node
+//        inOrder(node.right)
+//    }
+}
