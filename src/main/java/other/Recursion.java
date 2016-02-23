9c42bc93483c877fc17fff817f9bbbf1edf6b4d1
szelenin <szelenin@playtika.com>
2016-02-23 14:33:23 +0200

    Recursion - eliminated one recursion call

--- src/main/java/other/Recursion.java
+++ src/main/java/other/Recursion.java
@@ -19,18 +19,21 @@ public class Recursion {
     public static Integer find_val_or_next_smallest(TreeNode bst, int x) {
         //Get the greatest value <= x in a binary search tree.
         //Returns None if no such value can be found.
-        if (bst == null) {
-            return null;
-        } else if (bst.value() == x) {
-            return x;
-        } else if (bst.value() > x) {
-            return find_val_or_next_smallest(bst.left, x);
-        } else {
-            Integer right_best = find_val_or_next_smallest(bst.right, x);
-            if (right_best == null) {
-                return bst.value();
+        while (true) {
+
+            if (bst == null) {
+                return null;
+            } else if (bst.value() == x) {
+                return x;
+            } else if (bst.value() > x) {
+                bst = bst.left;
+            } else {
+                Integer right_best = find_val_or_next_smallest(bst.right, x);
+                if (right_best == null) {
+                    return bst.value();
+                }
+                return right_best;
             }
-            return right_best;
         }
     }
 
