a101b8f00bae00627e052e47949c9b7aa4aef733
Sergii_Zelenin <sergii_zelenin@epam.com>
2017-08-21 15:11:18 -0400

    Min change problem with memoization

--- src/main/java/other/Change.java
+++ src/main/java/other/Change.java
@@ -1,18 +1,27 @@
 package other;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Sergii_Zelenin on 8/21/2017.
  */
 public class Change {
+    private static Map<Integer, Integer> cache = new HashMap<>();
+
     public static int change(int amount, int[] coins) {
         //min(min(amount - coins[0])+1, min(amount - coins[1]) + 1 ...)
         if (amount <= 0) {
             return 0;
         }
+        if (cache.containsKey(amount)) {
+            return cache.get(amount);
+        }
         int minChange = Integer.MAX_VALUE;
         for (int coin : coins) {
             minChange = Math.min(change(amount - coin, coins) + 1, minChange);
         }
+        cache.put(amount, minChange);
         return minChange;
     }
 }
--- src/test/java/other/ChangeTest.java
+++ src/test/java/other/ChangeTest.java
@@ -8,6 +8,11 @@ import static org.junit.Assert.*;
  * Created by Sergii_Zelenin on 8/21/2017.
  */
 public class ChangeTest {
+    @Test(timeout = 1000)
+    public void testTimeout(){
+        assertEquals(20, Change.change(400, new int[]{20, 10, 5, 1}));
+    }
+
     @Test
     public void test(){
         assertEquals(2, Change.change(40, new int[]{20, 10, 5, 1}));
